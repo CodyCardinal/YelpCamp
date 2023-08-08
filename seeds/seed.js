d4b3cca28072c769e4423a7c48a10be1be036bff
Cody Cardinal <cardinalc@gmail.com>
2023-08-07 19:29:19 -0700

    CRUD Page styling complete.

--- seeds/seed.js
+++ seeds/seed.js
@@ -18,9 +18,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location : `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://images.unsplash.com/photo-1465800872432-a98681fc5828?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=400&q=60',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, consequuntur sed illo pariatur ab voluptate eum! Officiis quae repellat tempore, similique culpa nobis tempora, inventore, perferendis expedita natus ullam quaerat?',
+            price
         })
         await camp.save();
     }
@@ -29,3 +33,6 @@ const seedDB = async () => {
 seedDB().then(() => {
     mongoose.connection.close();
 });
+
+
+
