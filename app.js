920a68bdf35781a5aa781bbc0ddf4079ad80f960
Cody Cardinal <cardinalc@gmail.com>
2023-08-26 20:42:41 -0700

    Pre-Heroku Push

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
+// Heroku Prep
 // Run in production with NODE_ENV=production node app.js
 
 const express = require("express");
